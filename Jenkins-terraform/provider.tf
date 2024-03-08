4f20cba6416c67920486edbd235bd584fa7d9763
amir <greatamirulla@gmail.com>
2024-03-08 15:54:26 +0530

    code updated

--- Jenkins-terraform/provider.tf
+++ Jenkins-terraform/provider.tf
@@ -10,6 +10,6 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
-  access_key = "AKIA3FLDXR2TU4LOECO5"
-  secret_key = "0MiQX4iMz4vlgKZXZhreelueoVEXqc9XOhTvKCqRt"
+  
+  
 }
