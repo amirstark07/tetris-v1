624060d01f679e5f2e88ed7d797695c8e632767a
amirstark07 <147706121+amirstark07@users.noreply.github.com>
2024-03-19 12:26:30 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "amirstark07" # Replace with your actual S3 bucket name
+    bucket = "amir9698" # Replace with your actual S3 bucket name
     key    = "eks/terraform.tfstate"
     region = "us-east-1"
   }
